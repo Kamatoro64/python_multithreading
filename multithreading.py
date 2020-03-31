62f845709039ec9613461797ec2b94ddacc31159
NG Chester <zhentingchester.ng@murex.com>
2020-03-31 17:53:26 +0800

    Changing the application to use 2 threads using the threading module

--- multithreading.py
+++ multithreading.py
@@ -1,13 +1,27 @@
+import threading
 import time
 
 start = time.perf_counter()
 
-def do_something(): # Preferably I/O bound which will benefit from multithreading
+
+def do_something():  # Preferably I/O bound which will benefit from multithreading
     print('Sleeping 1 second...')
     time.sleep(1)
     print('Done Sleeping...')
 
-do_something()
+
+t1 = threading.Thread(target=do_something)
+t2 = threading.Thread(target=do_something)
+
+t1.start()
+t2.start()
+
+# Without join, the thread will begin execution and the script will proceed, stopping the clock before the threads
+# have finished executing
+
+t1.join()
+t2.join()
+
 
 finish = time.perf_counter()
 
