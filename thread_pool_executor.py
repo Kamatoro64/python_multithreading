befd5e8297e6c12502078646ddf61b67174140f2
NG Chester <zhentingchester.ng@murex.com>
2020-04-01 10:29:57 +0800

    Modifying list comprehension method to map method. Note that map method returns a list of results, instead of futures

--- thread_pool_executor.py
+++ thread_pool_executor.py
@@ -7,15 +7,21 @@ start = time.perf_counter()
 def do_something(seconds):  # Preferably I/O bound which will benefit from multi threading
     print(f'Sleeping {seconds} second(s)...')
     time.sleep(seconds)
-    return f'Done Sleeping...{seconds}' # Return instead of printing
+    return f'Done Sleeping...{seconds}'  # Return instead of printing
 
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
     secs = [5, 4, 3, 2, 1]
-    results = [executor.submit(do_something, sec) for sec in secs]
+    # results = [executor.submit(do_something, sec) for sec in secs]
 
-    for f in concurrent.futures.as_completed(results):
-        print(f.result())
+    # for f in concurrent.futures.as_completed(results):
+    #    print(f.result())
+
+    # Using the map method instead returns the results (in the order they are started, not futures
+    results = executor.map(do_something, secs)
+
+    for result in results:
+        print(result)
 
     # The submit method schedules a function to be executed and returns a futures object
     # However, instead of doing this 1 by 1, we use a loop above to spawn multiple threads
@@ -26,7 +32,6 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
 
     # print(f1.result())
     # print(f2.result())
-
 
 finish = time.perf_counter()
 
